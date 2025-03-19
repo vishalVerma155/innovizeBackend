896748ed17588632dd1c816bbed63b8679467af6
Vishal Verma <assistantjunior@earninghandle.com>
2025-03-19 16:45:41 +0530

    bug fix in contect us

--- controllers/contectUs.controllers.js
+++ controllers/contectUs.controllers.js
@@ -21,7 +21,6 @@ const createContectDetail = async (req, res) => {
             return res.status(400).json({ success: false, error: "Contect deatails not saved." });
         }
 
-        undefined
         return res.status(201).json({ success: true, message: "Message sent successfully", contactDetails: newContact });
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
